550f953830ab0a174957eb9195c9058bfe7bdfb7
Javier Botero <javierbotero10271@gmail.com>
2021-05-31 11:50:51 -0500

    reverse! interview cake

---
+++ interviewCake/reverse_string_in_place.rb
@@ -0,0 +1,59 @@
+def reverse!(string)
+  # Reverse the input string in place.
+  start = 0
+  final = string.length - 1
+
+  while start < final
+    string[start], string[final] = string[final], string[start]
+
+    start += 1
+    final -= 1
+  end
+
+  string
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Tests
+
+def run_tests
+  desc = 'empty string'
+  string = ''
+  reverse!(string)
+  expected = ''
+  assert_equal(string, expected, desc)
+
+  desc = 'single character string'
+  string = 'A'
+  reverse!(string)
+  expected = 'A'
+  assert_equal(string, expected, desc)
+
+  desc = 'longer string'
+  string = 'ABCDE'
+  reverse!(string)
+  expected = 'EDCBA'
+  assert_equal(string, expected, desc)
+end
+
+def assert_equal(a, b, desc)
+  puts "#{desc} ... #{a == b ? 'PASS' : "FAIL: #{a.inspect} != #{b.inspect}"}"
+end
+
+run_tests
